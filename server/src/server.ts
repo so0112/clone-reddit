4497a7931a1e0d58e5dd6c90ff1879601ce5abb9
so0112 <tkddh0112@gmail.com>
2022-09-04 17:56:00 +0900

    Add: 초기 셋팅

---
+++ server/src/server.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import morgan from "morgan";
+import { AppDataSource } from "./data-source";
+
+const app = express();
+app.use(express.json());
+app.use(morgan("dev"));
+
+// app.get url 접속하면 해당 블록의 코드 실행
+app.get("/", (_, res) => res.send("running"));
+
+// app.listen 포트로 접속하면 해당 블록의 코드 실행
+let port = 4000;
+app.listen(port, async () => {
+  console.log(`Server running at http://localhost:${port}`);
+
+  AppDataSource.initialize()
+    .then(() => {
+      console.log("database initialized");
+    })
+    .catch((error) => console.log(error));
+});
