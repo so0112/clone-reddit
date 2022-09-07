886e7c3a784ffcf61e9339dfb789241b659eb18f
so0112 <tkddh0112@gmail.com>
2022-09-07 23:56:51 +0900

    Add: register front 작성중

---
+++ client/src/pages/register.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const register = () => {
+  return (
+    <div className="bg-white">
+      <div className="flex flex-col itmes-center justify-content h-screen p-6">
+        <div className="w-10/12 mx-auto md:w-96">
+          <h1 className="mb-2 text-lg font-medium">회원가입</h1>
+          <form></form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default register;
