b2773aebf26f28f9569c3409e35b0253196f78da
so0112 <tkddh0112@gmail.com>
2022-09-09 23:16:23 +0900

    Feat: 회원가입 페이지 UI 생성하기

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import morgan from "morgan";
 import { AppDataSource } from "./data-source";
-
 const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
