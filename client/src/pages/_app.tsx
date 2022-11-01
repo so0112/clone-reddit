5886ff53946e16654e75c545218e97e9969ae04e
so0112 <tkddh0112@gmail.com>
2022-11-01 13:29:18 +0900

    Refactor: axios명 변경

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -2,17 +2,16 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Axios from 'axios';
 import { AuthProvider } from '../context/auth';
+import axios from 'axios';
 
 function MyApp({ Component, pageProps }: AppProps) {
-	// Next.js 환경변수 포맷(그냥 공식같은 거라 이해할 필요 x)
-	Axios.defaults.baseURL =
+	axios.defaults.baseURL =
 		process.env.NEXT_PUBLIC_SERVER_BASE_URL + '/api';
+	axios.defaults.withCredentials = true;
 	return (
-		<>
-			<AuthProvider>
-				<Component {...pageProps} />;
-			</AuthProvider>
-		</>
+		<AuthProvider>
+			<Component {...pageProps} />;
+		</AuthProvider>
 	);
 }
 
